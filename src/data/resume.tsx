667ee4a56ddcbc7919000fd1d9fd3f4995f5371b
zaheer-bashir <zaheerkpr3@gmail.com>
2024-07-02 22:56:55 +0500

    first commit

---
+++ src/data/resume.tsx
@@ -0,0 +1,368 @@
+import { Icons } from "@/components/icons";
+
+export const DATA = {
+  name: "Zaheer Bashir",
+  initials: "DV",
+  url: "https://dillion.io",
+  location: "San Francisco, CA",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer turned Entrepreneur. I love building things and helping people. Very active on Twitter.",
+  summary:
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+  avatarUrl: "/me.jpeg",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "JavaScript",
+    "MongoDb",
+    "Tailwind CSS",
+    "Styled Components",
+    "CSS3",
+    "HTML5",
+  ],
+  contact: {
+    email: "zaheerkpr3@gmail.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        url: "https://github.com/zaheer-bashir",
+        icon: Icons.github,
+      },
+      LinkedIn: {
+        url: "https://www.linkedin.com/in/zaheer-bashir/",
+        icon: Icons.linkedin,
+      },
+      X: {
+        url: "https://x.com/dillionverma",
+        icon: Icons.x,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Atomic Finance",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "Bitcoin Protocol Engineer",
+      logoUrl: "/atomic.png",
+      start: "May 2021",
+      end: "Oct 2022",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+    {
+      company: "Shopify",
+      badges: [],
+      href: "https://shopify.com",
+      location: "Remote",
+      title: "Software Engineer",
+      logoUrl: "/shopify.svg",
+      start: "January 2021",
+      end: "April 2021",
+      description:
+        "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
+    },
+    {
+      company: "Mitre Media",
+      href: "https://mitremedia.com/",
+      badges: [],
+      location: "Toronto, ON",
+      title: "Software Engineer",
+      logoUrl: "/mitremedia.png",
+      start: "May 2017",
+      end: "August 2017",
+      description:
+        "Designed and implemented a robust password encryption and browser cookie storage system in Ruby on Rails. Leveraged the Yahoo finance API to develop the dividend.com equity screener",
+    },
+  ],
+  education: [
+    {
+      school: "Buildspace",
+      href: "https://buildspace.so",
+      degree: "s3, s4, sf1, s5",
+      logoUrl: "/buildspace.jpg",
+      start: "2023",
+      end: "2024",
+    },
+    {
+      school: "University of Waterloo",
+      href: "https://uwaterloo.ca",
+      degree: "Bachelor's Degree of Computer Science (BCS)",
+      logoUrl: "/waterloo.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "Wilfrid Laurier University",
+      href: "https://wlu.ca",
+      degree: "Bachelor's Degree of Business Administration (BBA)",
+      logoUrl: "/laurier.png",
+      start: "2016",
+      end: "2021",
+    },
+    {
+      school: "International Baccalaureate",
+      href: "https://ibo.org",
+      degree: "IB Diploma",
+      logoUrl: "/ib.png",
+      start: "2012",
+      end: "2016",
+    },
+  ],
+  projects: [
+    {
+      title: "Chat Collect",
+      href: "https://chatcollect.com",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://chatcollect.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "",
+      video:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
+    },
+    {
+      title: "Magic UI",
+      href: "https://magicui.design",
+      dates: "June 2023 - Present",
+      active: true,
+      description:
+        "Designed, developed and sold animated UI components for developers.",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "PostgreSQL",
+        "Prisma",
+        "TailwindCSS",
+        "Stripe",
+        "Shadcn UI",
+        "Magic UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://magicui.design",
+          icon: <Icons.globe className="size-3" />,
+        },
+        {
+          type: "Source",
+          href: "https://github.com/magicuidesign/magicui",
+          icon: <Icons.github className="size-3" />,
+        },
+      ],
+      image: "",
+      video: "https://cdn.magicui.design/bento-grid.mp4",
+    },
+  ],
+  hackathons: [
+    {
+      title: "Hack Western 5",
+      dates: "November 23rd - 25th, 2018",
+      location: "London, Ontario",
+      description:
+        "Developed a mobile application which delivered bedtime stories to children using augmented reality.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-western.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2019/mlh-trust-badge-2019-white.svg",
+      links: [],
+    },
+    {
+      title: "Make School's Student App Competition 2017",
+      dates: "May 19th - 21st, 2017",
+      location: "International",
+      description: "Improved PocketDoc and submitted to online competition",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/make-school-hackathon.png",
+      win: "Top 10 Finalist | Honourable Mention",
+      links: [
+        {
+          title: "Medium Article",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://medium.com/make-school/the-winners-of-make-schools-student-app-competition-2017-a6b0e72f190a",
+        },
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+    {
+      title: "HackMining",
+      dates: "May 12th - 14th, 2017",
+      location: "Toronto, Ontario",
+      description: "Developed neural network to optimize a mining process",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/hack-mining.png",
+      links: [],
+    },
+    {
+      title: "Waterloo Equithon",
+      dates: "May 5th - 7th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed Pocketdoc, an app in which you take a picture of a physical wound, and the app returns common solutions or cures to the injuries or diseases.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/waterloo-equithon.png",
+      links: [
+        {
+          title: "Devpost",
+          icon: <Icons.globe className="h-4 w-4" />,
+          href: "https://devpost.com/software/pocketdoc-react-native",
+        },
+        {
+          title: "YouTube",
+          icon: <Icons.youtube className="h-4 w-4" />,
+          href: "https://www.youtube.com/watch?v=XwFdn5Rmx68",
+        },
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/pocketdoc-react-native",
+        },
+      ],
+    },
+    {
+      title: "SpaceApps Waterloo",
+      dates: "April 28th - 30th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed Earthwatch, a web application which allows users in a plane to virtually see important points of interest about the world below them. They can even choose to fly away from their route and then fly back if they choose. Special thanks to CesiumJS for providing open source world and plane models.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/space-apps.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/earthwatch",
+        },
+      ],
+    },
+    {
+      title: "MHacks 9",
+      dates: "March 24th - 26th, 2017",
+      location: "Ann Arbor, Michigan",
+      description:
+        "Developed Super Graphic Air Traffic, a VR website made to introduce people to the world of air traffic controlling. This project was built completely using THREE.js as well as a node backend server.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/mhacks-9.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/threejs-planes",
+        },
+      ],
+    },
+    {
+      title: "StartHacks I",
+      dates: "March 4th - 5th, 2017",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed at StartHacks 2017, Recipic is a mobile app which allows you to take pictures of ingredients around your house, and it will recognize those ingredients using ClarifAI image recognition API and return possible recipes to make. Recipic recieved 1st place at the hackathon for best pitch and hack.",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/starthacks.png",
+      win: "1st Place Winner",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source (Mobile)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/recipic-ionic",
+        },
+        {
+          title: "Source (Server)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/recipic-rails",
+        },
+      ],
+    },
+    {
+      title: "QHacks II",
+      dates: "February 3rd - 5th, 2017",
+      location: "Kingston, Ontario",
+      description:
+        "Developed a mobile game which enables city-wide manhunt with random lobbies",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/qhacks.png",
+      mlh: "https://s3.amazonaws.com/logged-assets/trust-badge/2017/white.svg",
+      links: [
+        {
+          title: "Source (Mobile)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/dillionverma/human-huntr-react-native",
+        },
+        {
+          title: "Source (API)",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/mattBlackDesign/human-huntr-rails",
+        },
+      ],
+    },
+    {
+      title: "Terrible Hacks V",
+      dates: "November 26th, 2016",
+      location: "Waterloo, Ontario",
+      description:
+        "Developed a mock of Windows 11 with interesting notifications and functionality",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/terrible-hacks-v.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/justinmichaud/TerribleHacks2016-Windows11",
+        },
+      ],
+    },
+    {
+      title: "Portal Hackathon",
+      dates: "October 29, 2016",
+      location: "Kingston, Ontario",
+      description:
+        "Developed an internal widget for uploading assignments using Waterloo's portal app",
+      image:
+        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/hackline/portal-hackathon.png",
+      links: [
+        {
+          title: "Source",
+          icon: <Icons.github className="h-4 w-4" />,
+          href: "https://github.com/UWPortalSDK/crowmark",
+        },
+      ],
+    },
+  ],
+} as const;
