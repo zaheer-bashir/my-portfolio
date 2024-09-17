7e22ab01f8a96a8047f186cf90d07b21677f37bb
zaheer-bashir <zaheerkpr3@gmail.com>
2024-09-17 11:22:47 +0500

    adjust layout width

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -154,7 +154,7 @@ export default function Page() {
               </div>
             </div>
           </BlurFade>
-          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+          <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[1000px] mx-auto">
             {DATA.projects.map((project, id) => (
               <BlurFade
                 key={project.title}
